98c85111e48630272c3a396e7bc66e1512b388e6
Diego Valles <dgvall134@gmail.com>
2022-09-22 17:57:00 -0600

    commit tree 3bcd721685b4680908728ecd3a2b80c7a5fbcade

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(nameString, emailString) {
+  return fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: nameString,
+      email: emailString,
+  })
+  })
+  .then(res => res.json())
+  .then(data => document.querySelector("body").append(data.id))
+  .catch(error => document.querySelector("body").append(`${error} User Could Not Be Created. Please try again.`))
+}
+submitData(`Diego`, `diego@gmail.com`)
